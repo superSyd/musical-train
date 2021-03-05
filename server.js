a60c82a36d303ea34ac30a226d4efb66f9fec36e
superSyd <37779021+superSyd@users.noreply.github.com>
2021-03-05 15:25:39 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt');
+const sha1 = require('sha1');
 const dns = require('dns');
 const mongoose = require('mongoose');
 const express = require('express');
@@ -60,11 +60,11 @@ const saveUrl = (currentUrl, hash) => {
         shortUrl: hash
       });
 
-     
-
           myUrl.save((err,data)=>{
           if(err){
-            return console.log(err)
+            return false
+          }else{
+            return true
           }
           });
 
@@ -74,29 +74,48 @@ app.post("/api/shorturl/new", (req,res)=>{
   const myOriginalUrl = req.body.url;
 
   dns.lookup(myOriginalUrl, (err, addresses, family)=>{
-    if(err){
-      console.log(err);
+    if(err.errno != 'ENOTFOUND'){
+      res.json({'error':'Invalid URL'})
     }
 
-    const currentUrl = err.hostname;
-    
+    });
 
-      bcrypt.hash(myOriginalUrl, 10, (err,hash)=>{
-      if(err){
-        console.log(err)
-      }
+    //const currentUrl = err.hostname;
 
-      const shortString = hash.substring(1,6)
+    const shortUrlOutput = sha1(myOriginalUrl)      
+        
+        if(saveUrl(myOriginalUrl, shortUrlOutput)){
+        res.json({
+        original_url: myOriginalUrl,
+        short_url: shortUrlOutput
+        })
+      } else {
+        
+       showExistingUrl(myOriginalUrl, res)
+      };
+  
       
-      saveUrl(currentUrl, shortString);
-      res.json({
-        originalUrl: currentUrl,
-        shortUrl: shortString
-      })
-
-      });
-  });
+  
 });
+
+const showExistingUrl = (currentUrl, res) =>{
+
+  sampleUrl.findOne({'originalUrl': currentUrl},(err,url)=>{
+    
+    if(err){
+      console.log(err)
+    }
+
+    res.json({
+        original_url: url.originalUrl,
+        short_url: url.shortUrl
+        })
+
+
+  })
+}
+  
+
 
 app.get("/api/shorturl/:shorturl",(req,res)=>{
   var myShortUrl = req.params.shorturl;
